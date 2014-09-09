85a424ecca1851d7f6f8d6981744d88e07efb724
ecgeiser <erica.geiser@gmail.com>
2014-09-09 12:11:03 -0400

    fixed issue with assign_image and made hunger default to 0

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,20 +1,23 @@
 class Pet < ActiveRecord::Base
 	belongs_to :user
 	has_one :pet_type
+	after_initialize :init
+
+	def init
+		self.hunger ||= 0
+	end
 
 	def assign_image
 		animal = self.pet_type_id
 		case self.hunger
 		when 3
-			image = PetType.photo_url_1
+			image = PetType.find(animal).photo_url_1
 		when 2
-			image = PetType.photo_url_2
+			image = PetType.find(animal).photo_url_2
 		when 1
-			image = PetType.photo_url_3
+			image = PetType.find(animal).photo_url_3
 		when 0
-			image = PetType.photo_url_4
-		else
-			image = "http://www.google.com"
+			image = PetType.find(animal).photo_url_4
 		end
 
 		self.update(image: image)
